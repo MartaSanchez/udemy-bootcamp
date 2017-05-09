1b1e0958fbdf07ee05f4c46cd269823473b2c67d
Marta Sanchez Rodriguez <martasr90@gmail.com>
2017-05-09 09:42:20 +0200

    function that replace all "-" with "_"

--- JAVASCRIPT/ejfunciones.js
+++ JAVASCRIPT/ejfunciones.js
@@ -1,15 +1,20 @@
 function isEven(num) {
   if (num % 2 ==0) {
-    return "True"
+    return "True";
   }
   else {
-    return "False"
+    return "False";
   }
-};
+}
 
 function factorial(n) {
   if ( n == 0 || n == 1) {
     return 1;
-  };
+  }
     return n * factorial(n-1);
+}
+
+function kebabToSnake(str) {
+  var newStr = str.replace(/-/g, "_");
+  return newStr;
 }
