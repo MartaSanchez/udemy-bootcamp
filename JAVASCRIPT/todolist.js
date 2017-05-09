b9770ab16210e20d1118181d2ebc78134ec9132c
Marta Sanchez Rodriguez <martasr90@gmail.com>
2017-05-09 18:19:49 +0200

    improved

--- JAVASCRIPT/todolist.js
+++ JAVASCRIPT/todolist.js
@@ -4,16 +4,35 @@ var input = prompt("What you would like to do?");
 
 while (input !== "quit") {
   if(input == "list") {
-    console.log(todo);
+    listToDo();
   }
   else if (input == "add"){
-    var newTodo = prompt("add a new to do");
-    todo.push(newTodo);
+    addToDo();
   }
   else if (input == "delete"){
-    todo.pop();
-    alert("You deleted the last item on the list");
+    deleteToDo();
   }
   input = prompt("What you would like to do?");
 }
 console.log("You quit the app");
+
+
+function listToDo() {
+  console.log("##########")
+  todo.forEach(function(todo, ind){
+    console.log(ind + ": " + todo);
+  });
+  console.log("##########")
+}
+
+function addToDo() {
+  var newTodo = prompt("add a new todo");
+  todo.push(newTodo);
+  alert("New todo added")
+}
+
+function deleteToDo() {
+  var index = prompt("Enter index of todo to delete");
+  todo.splice(index, 1);
+  alert("Deleted todo")
+}
