bf153a8a7839b405fd7511419869c3ac1b7f6694
Marta Sanchez Rodriguez <martasr90@gmail.com>
2017-05-16 18:19:09 +0200

    add reset button

--- JAVASCRIPT/colorgame.js
+++ JAVASCRIPT/colorgame.js
@@ -4,26 +4,22 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset")
+
+resetButton.addEventListener("click", function(){
+  resetButton.textContent = "New Colors"
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  h1.style.background = "#2b1d27";
+  message.textContent = "";
+  resetColors();
+
+});
 
 colorDisplay.textContent = pickedColor;
 
-
-for(var i = 0; i < squares.length; i++){
-  squares[i].style.background = colors[i];
-  squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.background;
-    if(clickedColor === pickedColor){
-      message.textContent = "Correct!";
-      colorChange(clickedColor);
-      h1.style.background = pickedColor;
-    }
-    else {
-      this.style.background = "#2b1d27";
-      message.textContent = "Try again!";
-    };
-  });
-}
-
+resetColors();
 
 function colorChange(color){
   for(var i = 0; i < squares.length; i++){
@@ -50,3 +46,23 @@ function generateColors(){
   var b = Math.floor(Math.random() * 256);
   return "rgb(" + r + ", "+ g + ", " + b + ")";
 };
+
+function resetColors(){
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.background = colors[i];
+    squares[i].addEventListener("click", function(){
+      var clickedColor = this.style.background;
+      if(clickedColor === pickedColor){
+        message.textContent = "Correct!";
+        colorChange(clickedColor);
+        h1.style.background = pickedColor;
+        resetButton.textContent = "Play again?";
+      }
+      else {
+        this.style.background = "#2b1d27";
+        message.textContent = "Try again!";
+      };
+    });
+  }
+
+}
