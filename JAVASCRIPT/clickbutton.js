6af006bd5c511fce5812395f40397f49bfe8ca2d
Marta Sanchez Rodriguez <martasr90@gmail.com>
2017-05-17 16:34:30 +0200

    Improved

--- JAVASCRIPT/clickbutton.js
+++ JAVASCRIPT/clickbutton.js
@@ -2,13 +2,18 @@ var b = document.querySelector("button");
 var c = document.querySelector("body");
 
 
-c.style.background = "white";
+c.style.background = randomColor();
 
 b.addEventListener("click", function(){
-  if(c.style.background == "white"){
-    c.style.background = "blue";
-  }
-  else {
-    c.style.background = "white"
-  }
-})
+  c.style.background = randomColor();
+});
+
+
+
+function randomColor(){
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  var color = "rgb(" + r + ", " + g + ", " + b + ")";
+  return color
+};
